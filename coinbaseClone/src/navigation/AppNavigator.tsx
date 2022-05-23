085fb8cb9edcfbb8603e000b37d8533845f8414d
jpromanonet <programming@jpromano.net>
2022-05-23 03:53:28 -0300

    Bottom navbar

--- coinbaseClone/src/navigation/AppNavigator.tsx
+++ coinbaseClone/src/navigation/AppNavigator.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator} from "react-native-screens/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
-import Home from '../screens/Home';
+import Home from '../screens/Home'
 import Settings from '../screens/Settings';
 import Actions from '../screens/Actions';
 import News from '../screens/News';
@@ -14,13 +14,22 @@ const TabBarNavigator = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return(
-        <TabBarNavigator.Navigator>
-            <TabBarNavigator.Screen name ="Home" component={Home}/>
-            <TabBarNavigator.Screen name ="Actions" component={Actions}/>
-            <TabBarNavigator.Screen name ="News" component={News}/>
-            <TabBarNavigator.Screen name ="Portfolio" component={Portfolio}/>
-            <TabBarNavigator.Screen name ="Prices" component={Prices}/>
-            <TabBarNavigator.Screen name ="Settings" component={Settings}/>
+        <TabBarNavigator.Navigator >
+            <TabBarNavigator.Screen name="Home" component={Home} />
+            <TabBarNavigator.Screen name="Portfolio" component={Portfolio} />
+            <TabBarNavigator.Screen name="Actions" component={Actions} />
+            <TabBarNavigator.Screen name="Prices" component={Prices} />
+            <TabBarNavigator.Screen name="Settings" component={Settings} />
         </TabBarNavigator.Navigator>
     )
 }
+
+const AppNavigator = () => {
+    return (
+        <NavigationContainer>
+            <TabNavigator />
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigator;
